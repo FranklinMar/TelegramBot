3d1d43009873c605c6814c85398d233b3b308dc0
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2022-01-03 18:23:00 +0200

    class

--- Classes.py
+++ Classes.py
@@ -1,42 +1,77 @@
-import geonamescache
-
 
 class Product:
-    def __init__(self, name, description, price, size, *args, **kwargs):
-        self.idn = 0
-
+    def __init__(self, idProduct, name, description, price, sex, image, type):
+        self.idProduct = idProduct
         self.name = name
         self.description = description
         self.price = price
-        self.size = size
-        self.image_list = []
+        self.sex = sex
+        self.image = image
+        self.type = type
 
+    @property
+    def idProduct(self):
+        return self.__idProduct
 
-class Basket:
-    def addProduct(self):
-        pass
+    @idProduct.setter
+    def idProduct(self, value):
+        self.__idProduct = value
 
-    def delProduct(self):
-        pass
+    @property
+    def name(self):
+        return self.__name
 
-    def payProducts(self):
-        pass
+    @name.setter
+    def name(self, value):
+        self.__name=value
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, value):
+        self.__description = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        self.__price = value
+
+    @property
+    def sex(self):
+        return self.__sex
+
+    @sex.setter
+    def sex(self, value):
+        self.__sex = value
+
+    @property
+    def image(self):
+        return self.__image
+
+    @image.setter
+    def image(self, value):
+        self.__image = value
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        self.__type = value
 
 
 class Profile:
-    def signIn(self):
-        pass
-
-    def changeData(self):
+    def __init__(self, id, name, surname, patronymic, country, city, post, delivery, card, date, phone_number):
         pass
 
 
-class Information:
-    def addResponse(self):
+class FullProduct:
+    def __init__(self, idFull, idProduct, size, color, count):
         pass
 
-    def viewResponses(self):
-        pass
-
-    def viewInformation(self):
-        pass
