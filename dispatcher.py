c51c9069391badcd1b36c1ad47a9a8ac0596527d
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2022-01-05 23:38:37 +0200

    dis

--- dispatcher.py
+++ dispatcher.py
@@ -7,5 +7,5 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 logging.basicConfig(level=logging.INFO)
 
 
-bot = Bot(token="5069171978:AAGVq-SqVTS6oD0zy0ECz6OZf6N2LHqqOTI", parse_mode=types.ParseMode.HTML)
+bot = Bot(token="2113090286:AAFXEnCPZIaQWBMhl9ohr5soUtXcKALMYqw", parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
