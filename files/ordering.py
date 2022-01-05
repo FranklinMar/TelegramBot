96a19e3144cff604dd83ced2c0766ca51b8caf7a
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2022-01-05 22:36:20 +0200

    commit

--- files/ordering.py
+++ files/ordering.py
@@ -2,7 +2,7 @@ import re
 
 from aiogram import types
 from dispatcher import dp, bot
-from DatabaseFunctions import select_by_id_db, select_by_id_db_full
+from DatabaseFunctions import select_by_id_db
 from Factory import Factory
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, KeyboardButton
 
@@ -21,19 +21,21 @@ async def process_order(message: types.Message):
 @dp.callback_query_handler(text="not_paid")
 async def show_orders(call: CallbackQuery):
     orders = factory.get_ordering(call.from_user.id, False)
+    price = 0
     if not orders:
         await call.message.answer("⚙️⚙️⚙️⚠️ It is so empty here... ⚠️ ⚙️⚙️⚙️")
     else:
         for order in orders:
             full_products = factory.get_full_product(order[1])
             product = select_by_id_db(full_products[0][1])
+            price += product[3]*order[4]
             await bot.send_photo(call.from_user.id, photo=product[5])
             await bot.send_message(call.from_user.id,
                                    f'{product[1]}\nDescription: {product[2]}\nPrice: {product[3] * order[4]} Color: {full_products[0][3]} Size: {full_products[0][2]} Count: {order[4]}',
                                    reply_markup=InlineKeyboardMarkup().add(
                                        InlineKeyboardButton(f'Remove from order ❌',
                                                             callback_data=f'Del {order[0]}')))
-            await call.message.answer("Для оплати натисніть кнопку⬇️", reply_markup=ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Pay")))
+            await call.message.answer(f"Загальна сума замовлення становить:{price}\nДля оплати натисніть кнопку⬇️", reply_markup=ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Pay")))
 
 
 @dp.callback_query_handler(lambda c: re.match('Del [0-9]+', c.data))
