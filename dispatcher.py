e9bf84eeed185dbd94be3320329bf2a68de0198f
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2022-01-06 04:41:56 +0200

    new

--- dispatcher.py
+++ dispatcher.py
@@ -6,6 +6,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 logging.basicConfig(level=logging.INFO)
 
+PAYMENTS_PROVIDER_TOKEN = ''
 
-bot = Bot(token="2113090286:AAFXEnCPZIaQWBMhl9ohr5soUtXcKALMYqw", parse_mode=types.ParseMode.HTML)
+bot = Bot(token="", parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
