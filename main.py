09d58b43b2816534e24724544fad3cd46a9e080f
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2022-01-07 02:42:41 +0200

    changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from aiogram.utils import executor
 from dispatcher import dp
 from files.delete_ordering import on_startup
-
+from files.payment import loop
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup, loop=loop)
