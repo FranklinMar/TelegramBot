97116fdc38d3525389824cdcb32e9576d00b0796
denisbereznuuk <denisbereznuuk@gmail.com>
2022-01-13 00:51:46 +0200

    Basket Update V2.1

--- dispatcher.py
+++ dispatcher.py
@@ -8,6 +8,6 @@ logging.basicConfig(level=logging.INFO)
 
 PAYMENTS_PROVIDER_TOKEN = '1744374395:TEST:7c8b4dfb1271ef0a0486'
 
-bot = Bot(token="5064597537:AAHxPj8pwNhjLMmtaKWnae1evxOvqqrn-t4", parse_mode=types.ParseMode.HTML)
+bot = Bot(token="", parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 factory = Factory("database.db")
